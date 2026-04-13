de42ea15f6fc0397e4edec3eaae0ccf696b58a90
minhnhatdanchoi <minh18052003@gmail.com>
2026-04-13 22:17:49 +0700

    Update basePath for new repo name gioithieunganhtruyenthong

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/studio',
+  basePath: '/gioithieunganhtruyenthong',
   typescript: {
     ignoreBuildErrors: true,
   },
